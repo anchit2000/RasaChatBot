0b31e5f6656829a8ff4a20b0aafa5791b0d69fc7
Anchit Shrivastava <anchit2000@pop-os.localdomain>
2022-05-12 20:21:37 +0530

    tried adding phone number

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import random
+
+
+class ActionSuggestPlaces(Action):
+
+    def name(self) -> Text:
+        return "action_suggest_places"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        destination_value = tracker.get_slot("destination")
+
+        if not destination_value:
+
+            places = ["Merida", "Bilbao", "Salamanca", "Cuenca", "Ibiza", "Segovia", "Ronda", "Santiago de Compostela",
+                      "Toledo", "Cordoba", "San Sebastian", "Valencia", "Seville", "Madrid", "Mallorca", "Barcelona",
+                      "Granada", ]
+
+            dispatcher.utter_message(text=f"You can visit {random.choice(places)}")
+
+        else:
+            dispatcher.utter_message(text=f"{destination_value} is a great place!")
+
+        return []
